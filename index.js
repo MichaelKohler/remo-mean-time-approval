e8d684c254e7045a5a34c20cffb5aa2972b6b6fa
Michael Kohler <mkohler@picobudget.com>
2016-09-30 18:17:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+var bz = require('bz');
+var _ = require('lodash');
+var allBugs = require('./bugs.json');
+var fs = require('fs-extra');
+var async = require('async');
+var config = require('./config.js');
+
+var bugzilla = bz.createClient({
+  url: "https://bugzilla.mozilla.org/rest/",
+  username: config.user,
+  password: config.password,
+  timeout: 30000
+});
+
+var allBugsMeanTimes = [];
+
+var params = {
+  component: 'Budget Requests',
+  product: 'Mozilla Reps',
+  status: ['ASSIGNED', 'RESOLVED'],
+  resolution: ['FIXED', 'INVALID', 'WONTFIX', '---']
+};
+
+console.log(allBugs.length);
+
+if (!allBugs || allBugs.length === 0) {
+  console.log('starting to fetch bugs...');
+
+  bugzilla.searchBugs(params, function(error, bugs) {
+    if (error) {
+      console.log(error);
+    } else {
+      fs.outputJson('bugs.json', bugs, function (err) {
+        if (err) console.log(err);
+
+        processHistoryForAllBugs(bugs);
+      });
+    }
+  });
+} else {
+  processHistoryForAllBugs(allBugs);
+}
+
+function processHistoryForAllBugs(bugs) {
+  console.log('Number of bugs: ', bugs.length);
+  async.eachSeries(bugs, function (bug, callback) {
+    console.log(bug);
+
+    console.log('fetching history for ', bug.id);
+
+    bugzilla.bugHistory(bug.id, function (error, history) {
+      if (error) console.log(error);
+
+      var approvalRequests = [];
+      var approved = [];
+      var histories = history[0].history;
+
+      _.each(histories, function (historyObject) {
+        _.each(historyObject.changes, function (change) {
+          if (change.field_name === 'flagtypes.name' && change.added.includes('remo-approval?')) {
+            console.log('we have a flag for approval?');
+
+            var approvalRequestHistory = _.cloneDeep(historyObject);
+            approvalRequests.push(approvalRequestHistory);
+          }
+
+          if (change.field_name === 'flagtypes.name' && change.added.includes('remo-approval+')) {
+            console.log('we have a flag for approval+');
+
+            var approvedHistory = _.cloneDeep(historyObject);
+            approved.push(approvedHistory);
+          }
+
+          if (approvalRequests.length > 0 && approved.length > 0) {
+            var timeRequest = new Date(approvalRequests[approvalRequests.length - 1].when);
+            console.log('timeRequest', timeRequest);
+
+            var timeApproval = new Date(approved[approved.length - 1].when);
+            console.log('timeApproval', timeApproval);
+
+            var timeDifference = timeApproval - timeRequest;
+            console.log('difference', timeDifference);
+
+            var requestDifference = {
+              bugID: bug.id,
+              status: bug.status,
+              resolution: bug.resolution,
+              bugSummary: bug.summary,
+              difference: timeDifference,
+              approver: approvalRequests[approvalRequests.length - 1].who
+            };
+
+            allBugsMeanTimes.push(requestDifference);
+
+            console.log('approvalRequests');
+            console.log((require('util')).inspect(approvalRequests, showHidden=false, depth=10, colorize=true));
+            console.log('approved');
+            console.log((require('util')).inspect(approved, showHidden=false, depth=10, colorize=true));
+
+            console.log('finished thing');
+            console.log((require('util')).inspect(allBugsMeanTimes, showHidden=false, depth=10, colorize=true));
+            process.exit();
+          }
+        });
+      });
+
+      callback();
+    });
+  }, function (err) {
+    console.log('finished...');
+    console.log((require('util')).inspect(allBugsMeanTimes, showHidden=false, depth=10, colorize=true));
+
+    fs.outputJson('bugs.json', bugs, function (err) {
+      if (err) console.log(err);
+
+      processHistoryForAllBugs(bugs);
+    });
+  });
+}
